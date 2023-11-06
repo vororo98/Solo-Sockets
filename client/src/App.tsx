4e64d2d826c723635578eb7cd431b08ef9a3b02a
vororo <adamolsson@outlook.com>
2023-11-06 23:55:52 +0900

    still fixing errors 2!

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
 
   // Create WebSocket connection. Define behaviour.
   useEffect(() => {
-    socketRef.current = new WebSocket("wss://https://super-robot-programmer.onrender.com//echo");
+    socketRef.current = new WebSocket("wss://super-robot-programmer.onrender.com//echo");
     const socket = socketRef.current;
 
      // Connection opened
